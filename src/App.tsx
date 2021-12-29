e21182ec4f98817e4065f9f5be8315e44da66d7b
Grit <perepadin00@gmail.com>
2021-12-29 19:44:38 +0300

    complete TopSection and App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import tw from 'twin.macro';
 import './App.css';
+import { Footer } from './components/footer';
+import { ExploreSection } from './containers/ExploreSection';
 import { TopSection } from './containers/TopSection';
 
 const AppContainer = tw.div`
   flex
+  flex-col
   w-full
   h-full
+  overflow-hidden
 `;
 
 function App() {
   return <AppContainer>
     <TopSection />
+    <ExploreSection />
+    <Footer />
   </AppContainer>
 }
 
